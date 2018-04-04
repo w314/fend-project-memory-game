1c19cea232673b33c57d907671b64957cf812446
Piroska Werner <piroska.werner@gmail.com>
2018-04-04 07:24:08 -0500

    feat: Add cards array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 /*
  * Create a list that holds all of your cards
  */
-
+ const cards = [
+   'fa-dimond',
+   'fa-paper-plane-o',
+   'fa-anchor',
+   'fa-bolt',
+   'fa-cube',
+   'fa-leaf',
+   'fa-bicycle',
+   'fa-bomb',
+   'fa-dimond',
+   'fa-paper-plane-o',
+   'fa-anchor',
+   'fa-bolt',
+   'fa-cube',
+   'fa-leaf',
+   'fa-bicycle',
+   'fa-bomb'
+ ];
 
 /*
  * Display the cards on the page
